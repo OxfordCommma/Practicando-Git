37ea397ccbce740ac1640bb738b34853bd30b302
RamiroElianfa <ramirofarias4@gmail.com>
2024-01-04 19:13:20 -0300

    6TA

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,3 @@
 print("New Hello git! v2 OMG")
 print("Pero ahora desde GitHub!!!")
-print("2da prueba 3ra y ahora la ultima 4° y ahora la 5ta")
+print("2da prueba 3ra y ahora la ultima 4° y ahora la 5ta la 6tga")
